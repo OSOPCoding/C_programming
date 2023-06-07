0fd10cf8007f02437c369bb55a2af1e1cf3bd476
osop <osopcoding@gmail.com>
2023-06-07 06:41:46 +0100

    2 realse

--- 1_2__4_7_11_16.c
+++ 1_2__4_7_11_16.c
@@ -1,16 +1,10 @@
 #include<stdio.h>
-int main()
+
+void main()
 {
-     int n,i,j;
-     printf("Enter any number\n");// Prompt user to enter any number
-     scanf("%d",&n);// read value for n from user
-     i = 1;
-     j = 1;
-     while(i<=n)// Here we are printing 1 2 4 7 11 series with the use of while loop
-     {
-         printf("%d\n",j);
-         j = j+i;
-         i = i+1;
-     }
-     return 0;
+    int a =10;
+    short int *ptr ;
+    ptr = &a;
+    printf("%d\n",ptr);
+    printf("%u",ptr);
 }
